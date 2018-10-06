ddf95f2fc72351d1491df511504dbfb6b43c92f9
Elena Grahovac <elena@grahovac.me>
2018-10-06 12:07:23 +0300

    Added env configuration to deal with ports

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -4,26 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/rumyantseva/go-sofia/internal/diagnostics"
 )
 
 func main() {
-	log.Print("Hello, World")
+	log.Print("Starting the application...")
+
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
+
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		log.Fatal("The diagnostics port should be set")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
 	go func() {
-		err := http.ListenAndServe(":8080", router)
+		err := http.ListenAndServe(":"+blPort, router)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
 	diagnostics := diagnostics.NewDiagnostics()
-	err := http.ListenAndServe(":8585", diagnostics)
+	err := http.ListenAndServe(":"+diagPort, diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
