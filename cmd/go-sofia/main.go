6d2af5c5ccf4cb72087f1e6817a1a40ab6eef155
Elena Grahovac <elena@grahovac.me>
2018-10-06 11:28:58 +0300

    Added router

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Print("Hello, World")
 
-	err := http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
